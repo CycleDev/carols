d262541667308565a9e7238481b585acff71efac
Nazar Oprysk <noprysk@Nazars-MacBook-Pro.local>
2014-12-01 21:13:14 +0200

    app icons

--- carols/src/main/java/nao/cycledev/carols/utils/ScreenUtils.java
+++ carols/src/main/java/nao/cycledev/carols/utils/ScreenUtils.java
@@ -5,12 +5,8 @@ import android.content.res.Configuration;
 
 public class ScreenUtils {
 
-    // For test
-    public static boolean isScreenSizeLarge() {
-        return true;
-    }
-
     public static boolean isScreenSizeLarge(Context context) {
-        return ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE);
+        return ((context.getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE);
     }
 }
